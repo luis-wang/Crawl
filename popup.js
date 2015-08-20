1a36a55100d06558084f78d33549f185b6f1f883
Joris ten Tusscher <joristt@gmail.com>
2015-08-20 23:37:20 +0200

    Added main menu + some animation sweetness.

--- popup.js
+++ popup.js
@@ -1,1 +1,21 @@
-chrome.tts.speak("hah gay!!!!!");
+// chrome.tts.speak("hah gay!!!!!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+	// $('body').css('width', '350px');
+
+    var createDatabaseBtn = $('#createDatabaseBtn');
+
+	createDatabaseBtn.on('click', function() {
+
+		$('#container').addClass('fadeOut');
+
+		setTimeout(addResizeBodyClass, 300)
+
+		function addResizeBodyClass() {
+			$('body').addClass('largeBody');
+		}
+
+	});
+
+});
