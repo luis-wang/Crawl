65d9f51a087ed30fb8d072124d3501716a68f408
Portrius <rmriesmeijer@gmail.com>
2015-08-20 23:38:53 +0200

    content update
    right element selection and highlighting

--- content.js
+++ content.js
@@ -1,13 +1,19 @@
+//add event listener for element selection click
+document.addEventListener("click", printMousePos);
 
+function printMousePos(e) {
+    //get target element and print xpath of the element
+    var elementMouseIsOver = e.target;
+    var x = getXPath(elementMouseIsOver);
+    $(elementMouseIsOver).css("text-shadow","0 0 3px #0dcaff");
+}
 
-function getXPath( element )
+//standard element object to xpath string function
+function getXPath(element)
 {
-var val=element.value;
-    //alert("val="+val);
+	var val=element.value;
     var xpath = '';
-    for ( ; element && element.nodeType == 1; element = element.parentNode )
-    {
-        //alert(element);
+    for (; element && element.nodeType == 1; element = element.parentNode) {
         var id = $(element.parentNode).children(element.tagName).index(element) + 1;
         id > 1 ? (id = '[' + id + ']') : (id = '');
         xpath = '/' + element.tagName.toLowerCase() + id + xpath;
