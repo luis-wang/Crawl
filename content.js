39f6a2e9b5b6b93a6fb6d209b91586b64fd08366
Joris ten Tusscher <joristt@gmail.com>
2015-08-21 02:18:37 +0200

    added code that prevents hyperlinks from opening and hover effects from showing, so that when people select text in (for example) a hyperlink, they won’t be redirected to a different page.

--- content.js
+++ content.js
@@ -1,16 +1,25 @@
+//Cancel all mouseover and click events; when a user clicks (for example) a hyperlink,
+//we wan't the link to be selected, but we don't want the href to be opened.
+$('body').on('click', function(e) {
+    e.preventDefault();
+});
+$('body').on('mouseover', false);
+
 //add event listener for element selection click
 document.addEventListener("click", printMousePos);
 
 function printMousePos(e) {
+    
     //get target element and print xpath of the element
     var elementMouseIsOver = e.target;
     var x = getXPath(elementMouseIsOver);
     $(elementMouseIsOver).css("text-shadow","0 0 3px #0dcaff");
+
 }
 
 //standard element object to xpath string function
-function getXPath(element)
-{
+function getXPath(element) {
+
 	var val=element.value;
     var xpath = '';
     for (; element && element.nodeType == 1; element = element.parentNode) {
@@ -19,4 +28,5 @@ function getXPath(element)
         xpath = '/' + element.tagName.toLowerCase() + id + xpath;
     }
     return xpath;
+
 }
