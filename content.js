5ff2df118fb4f1f6fea6999f15fa8f75e4fcb222
Joris ten Tusscher <joristt@gmail.com>
2015-08-24 01:18:30 +0200

    added basic scraper code

--- content.js
+++ content.js
@@ -1,26 +1,59 @@
+function getTitle(PassedXPath, HTMLDocument) {
+    return document.evaluate(PassedXPath, HTMLDocument, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerHTML;
+}
+
+function getInnerHTMLFromXPath(part1) {
+
+    var pageHTML;
+    $.get('https://tweakers.net/categorie/215/smartphones/producten/#filter:q1YqSExPVbIy1AEzgjOrQBwDg1oA', function(pageHTML) {
+
+        var parser = new DOMParser();
+        var HTMLDocument = parser.parseFromString(pageHTML, "text/html");
+
+        var title = getTitle(part1, HTMLDocument);
+        console.log(title);
+
+    }, 'html');
+
+}
+
+getInnerHTMLFromXPath('/html/body[@class="fixedHeader hasTracker smallTracker"]/div[@id="layout"]/div[@id="contentArea"]/div[@class="contentArea"]/div[@id="tab:producten"]/div[@class="productFilter productCompare responsiveFilter"]/div[@id="listingContainer"]/div[@id="listing"]/form[@id="compareProductListing"]/table[@class="listing useVisitedState"]/tbody/tr/td[@class="itemname"]/p[@class="ellipsis"]/a');
+getInnerHTMLFromXPath('/html/body/div[2]/div/div[2]/div/div/div[2]/div/form/table/tbody/tr/td[3]/p/a');
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+function(request, sender, sendResponse) {
         if (request.message == "addColumnToNewDatabase") {
-            // sendResponse({message: "readyToAddColumn"});
+            sendResponse({message: "readyToAddColumn"});
         }
-});
+    }
+);
 
 //Cancel all mouseover and click events; when a user clicks (for example) a hyperlink,
 //we want the link to be selected, but we don't want the hyperlink reference to be opened.
-//$('body').on('click', function(e) {
-//    e.preventDefault();
-//});
-//$('body').on('mouseover', false);
+$('body').on('click', function(e) {
+   e.preventDefault();
+});
+$('body').on('mouseover', false);
+
+var firstClick;
 
 //add event listener for element selection click
 document.addEventListener('click', printMousePos);
 
 //get target element and print xpath of the element
 function printMousePos(e) {
-    var elementMouseIsOver = e.target;
-    var x = getXPath(elementMouseIsOver);
-    $(elementMouseIsOver).css('box-shadow', '0 0 5px #0dcaff');
-    alert(x);
+
+    if (firstClick == null) {
+        firstClick = getXPath(e.target);
+        alert(firstClick);
+    }
+    else {
+        var secondClick = firstClick = getXPath(e.target);
+        alert(secondClick);
+        alert(matchShortestCommonXPath(firstClick, secondClick));
+    }
+
+    // $(elementMouseIsOver).css('box-shadow', '0 0 5px #0dcaff');
 }
 
 /*
@@ -54,8 +87,7 @@ function getXPath(targetNode) {
 
 //xpath segment lookup
 function getXPath(targetNode) {
-    for(var xpathSegments = []; targetNode != null && targetNode.nodeType == 1; targetNode = targetNode.parentNode)
-    {
+    for(var xpathSegments = []; targetNode != null && targetNode.nodeType == 1; targetNode = targetNode.parentNode) {
         //if it has an id make the xpath specified to the id else class else clean tagname
         if(targetNode.hasAttribute('id')) {
             xpathSegments.push(targetNode.localName.toLowerCase() + '[@id="' + targetNode.getAttribute('id') + '"]');
@@ -85,8 +117,8 @@ function segmentsToXPathText(xpathSegments) {
 
 //give xpath text that describes 2 target nodes
 function matchShortestCommonXPath(firstTarget, secondTarget) {
-    var xpathSegmentsFirst = getXPath(firstTarget);
-    var xpathSegmentsSecond = getXPath(secondTarget);
+    var xpathSegmentsFirst = firstTarget;
+    var xpathSegmentsSecond = secondTarget;
     var result = [];
     for(var i = 0; i < xpathSegmentsFirst.length && i < xpathSegmentsSecond.length; i++) {
         if(xpathSegmentsFirst[i] == xpathSegmentsSecond[i])
--- pages/createdb/script.js
+++ pages/createdb/script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	$('#addFieldBtn').on('click', function() {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         	chrome.tabs.sendMessage(tabs[0].id, {message: "addColumnToNewDatabase"}, function(response) {
+        		console.log(response.message);
         	});
    		});
 	});
