72cd2fbea477c6ab6f5ea654ba66b729aaf06cbc
Joris ten Tusscher <joristt@gmail.com>
2015-08-23 13:45:36 +0200

    removed background.js. removed random javascript stuff.

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message == "addColumnToNewDatabase") {
-            sendResponse({message: "readyToAddColumn"});
-
+            // sendResponse({message: "readyToAddColumn"});
         }
 });
 
--- pages/createdb/script.js
+++ pages/createdb/script.js
@@ -16,9 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	$('#addFieldBtn').on('click', function() {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         	chrome.tabs.sendMessage(tabs[0].id, {message: "addColumnToNewDatabase"}, function(response) {
-        		if (response.message == "readyToAddColumn") {
-        			alert('bye');
-        		}
         	});
    		});
 	});
