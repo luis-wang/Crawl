a44b37ddcc318f3b853e0201eaa7a71fbc3438fa
Joris ten Tusscher <joristt@gmail.com>
2015-08-24 16:08:19 +0200

    first version of working crawler.

--- content.js
+++ content.js
@@ -1,27 +1,32 @@
-function getTitle(PassedXPath, HTMLDocument) {
-    return document.evaluate(PassedXPath, HTMLDocument, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerHTML;
+function getInnerHTMLFromXPath(PassedXPath, HTMLDocument) {
+    
+    var result = document.evaluate(PassedXPath, HTMLDocument, null, XPathResult.ANY_TYPE, null);
+
+    var nodes = [];
+    for (var node = result.iterateNext(); node != null; node = result.iterateNext()) {
+        nodes.push(node.textContent);
+    }
+
+    return nodes;
+
 }
 
-function getInnerHTMLFromXPath(part1) {
+function Crawl(XPath) {
 
-    var pageHTML;
-    $.get('https://tweakers.net/categorie/215/smartphones/producten/#filter:q1YqSExPVbIy1AEzgjOrQBwDg1oA', function(pageHTML) {
+    $.get(document.URL, function(HTMLString) {
 
         var parser = new DOMParser();
-        var HTMLDocument = parser.parseFromString(pageHTML, "text/html");
+        var HTMLDocument = parser.parseFromString(HTMLString, "text/html");
 
-        var title = getTitle(part1, HTMLDocument);
+        var title = getInnerHTMLFromXPath(XPath, HTMLDocument);
         console.log(title);
 
     }, 'html');
 
 }
 
-getInnerHTMLFromXPath('/html/body[@class="fixedHeader hasTracker smallTracker"]/div[@id="layout"]/div[@id="contentArea"]/div[@class="contentArea"]/div[@id="tab:producten"]/div[@class="productFilter productCompare responsiveFilter"]/div[@id="listingContainer"]/div[@id="listing"]/form[@id="compareProductListing"]/table[@class="listing useVisitedState"]/tbody/tr/td[@class="itemname"]/p[@class="ellipsis"]/a');
-getInnerHTMLFromXPath('/html/body/div[2]/div/div[2]/div/div/div[2]/div/form/table/tbody/tr/td[3]/p/a');
-
 chrome.runtime.onMessage.addListener(
-function(request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
         if (request.message == "addColumnToNewDatabase") {
             sendResponse({message: "readyToAddColumn"});
         }
@@ -35,23 +40,16 @@ $('body').on('click', function(e) {
 });
 $('body').on('mouseover', false);
 
-var firstClick;
-
 //add event listener for element selection click
 document.addEventListener('click', printMousePos);
 
 //get target element and print xpath of the element
 function printMousePos(e) {
 
-    if (firstClick == null) {
-        firstClick = getXPath(e.target);
-        alert(firstClick);
-    }
-    else {
-        var secondClick = firstClick = getXPath(e.target);
-        alert(secondClick);
-        alert(matchShortestCommonXPath(firstClick, secondClick));
-    }
+    var secondClick = getXPath(e.target);
+    var finalxpath = matchShortestCommonXPath(secondClick);
+    alert(finalxpath);
+    Crawl(finalxpath);
 
     // $(elementMouseIsOver).css('box-shadow', '0 0 5px #0dcaff');
 }
@@ -87,16 +85,16 @@ function getXPath(targetNode) {
 
 //xpath segment lookup
 function getXPath(targetNode) {
-    for(var xpathSegments = []; targetNode != null && targetNode.nodeType == 1; targetNode = targetNode.parentNode) {
+    for(var xpathSegments = []; targetNode != null && targetNode.localName != 'body' && targetNode.nodeType == 1; targetNode = targetNode.parentNode) {
         //if it has an id make the xpath specified to the id else class else clean tagname
-        if(targetNode.hasAttribute('id')) {
-            xpathSegments.push(targetNode.localName.toLowerCase() + '[@id="' + targetNode.getAttribute('id') + '"]');
+        if (targetNode.hasAttribute('id')) {
+                xpathSegments.push(targetNode.localName + '[@id="' + targetNode.getAttribute('id') + '"]');
         }
-        else if(targetNode.hasAttribute('class')) {
-            xpathSegments.push(targetNode.localName.toLowerCase() + '[@class="' + targetNode.getAttribute('class') + '"]');
+        else if (targetNode.hasAttribute('class')) {
+                xpathSegments.push(targetNode.localName + '[@class="' + targetNode.getAttribute('class') + '"]');
         }
         else {
-            xpathSegments.push(targetNode.localName.toLowerCase());
+            xpathSegments.push(targetNode.localName);
         }
     }
     //join the segments to get xpath text
@@ -109,22 +107,17 @@ function getXPath(targetNode) {
 //make normal text out of segment array
 function segmentsToXPathText(xpathSegments) {
     xpathSegments.reverse();
-    if(xpathSegments.length > 0)
-        return '/' + xpathSegments.join('/');
+    if (xpathSegments.length > 0)
+        return '//' + xpathSegments.join('/');
     else
         return null;
 }
 
 //give xpath text that describes 2 target nodes
-function matchShortestCommonXPath(firstTarget, secondTarget) {
-    var xpathSegmentsFirst = firstTarget;
-    var xpathSegmentsSecond = secondTarget;
+function matchShortestCommonXPath(xpathSegments) {
     var result = [];
-    for(var i = 0; i < xpathSegmentsFirst.length && i < xpathSegmentsSecond.length; i++) {
-        if(xpathSegmentsFirst[i] == xpathSegmentsSecond[i])
-            result.push(xpathSegmentsFirst[i]);
-        else
-            break;
+    for(var i = 0; i < xpathSegments.length; i++) {
+        result.push(xpathSegments[i]);
     }
     var resulttext = segmentsToXPathText(result);
     return resulttext;
